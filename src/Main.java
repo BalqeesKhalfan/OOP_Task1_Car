d0f048475e4572e16bd792560e973290034dcfa7
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-03-28 11:30:07 +0400

    create car class using oop

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        Car car=new Car();
+        System.out.println("Enter Make of Car:");
+        String make=sr.nextLine();
+        System.out.println("Enter Model :");
+        String model=sr.nextLine();
+        System.out.println();
+
+    }
+}
